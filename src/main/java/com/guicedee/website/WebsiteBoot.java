cfe3da5a6bc9c9cb581caf4facc527a4095ecbc5
TheBlueHorizon <dtweninger81@gmail.com>
2026-03-20 13:06:58 +0200

    Refactor `WebsiteBoot` to integrate `WaPage` layout, update tab structure, and enhance navigation logic.

--- src/main/java/com/guicedee/website/WebsiteBoot.java
+++ src/main/java/com/guicedee/website/WebsiteBoot.java
@@ -5,6 +5,11 @@ import com.jwebmp.core.base.angular.client.annotations.routing.NgRoutable;
 import com.jwebmp.core.base.angular.client.services.interfaces.INgComponent;
 import com.jwebmp.core.base.angular.services.RouterOutlet;
 import com.jwebmp.core.base.html.DivSimple;
+import com.jwebmp.webawesome.components.button.WaButton;
+import com.jwebmp.webawesome.components.button.WaDropDown;
+import com.jwebmp.webawesome.components.button.WaDropdownItem;
+import com.jwebmp.webawesome.components.divider.WaDivider;
+import com.jwebmp.webawesome.components.icon.WaIcon;
 import com.jwebmp.webawesome.components.tabgroup.WaTab;
 import com.jwebmp.webawesome.components.tabgroup.WaTabGroup;
 import com.jwebmp.webawesome.components.tabgroup.WaTabPanel;
@@ -18,47 +23,44 @@ public class WebsiteBoot extends DivSimple<WebsiteBoot> implements INgComponent<
         addStyle("width:100%");
         addStyle("height:100%");
 
-        add(new WaTabGroup<>()
-                .addStyle("height:100%")
+        addClass("wa-dark");
+
+        //this has the menu bar menu and header
+        WaPage<?> page = new WaPage<>();
+        page.setNavOpen(true);
+
+        var menu = page.getMenu();
+        menu.add("Home");
+
+        var navigation = page.getNavigation();
+        navigation.add("GuicedEE Navigation Links");
+
+        var header = page.getHeader();
+        header.add("GuicedEE Website");
+
+        var mainHeader = page.getMainHeader();
+        var main = page.getMain();
+        main.add(new RouterOutlet());
+
+
+        // Tabs
+        WaTabGroup<?> tabs = new WaTabGroup<>()
                 .addStyle("width:100%")
-                .add(new WaTab<>().setPanel("guicedPanel").setName("GuicedEE").setText("GuicedEE").setActiveTabColor("#FF007F"))
-                .add(new WaTabPanel<>().setName("GuicedEE").add(new RouterOutlet()))
-                .add(new WaTab<>().setPanel("jwebmpPanel").setName("JWebMP").setText("JWebMP").setActiveTabColor("#FF007F"))
-                .add(new WaTabPanel<>().setName("JWebMP").add("This should be a new url redirect"))
-                .add(new WaTab<>().setPanel("entityPanel").setName("Entity Assist").setText("Entity Assist").setActiveTabColor("#FF007F"))
-                .add(new WaTabPanel<>().setName("Entity Assist").add("This should be a new url redirect"))
-                .add(new WaTab<>().setPanel("activityPanel").setName("Activity Master").setText("Activity Master").setActiveTabColor("#FF007F"))
-                .add(new WaTabPanel<>().setName("Activity Master").add("This should be a new url redirect"))
-        );
+                .addStyle("height:100%");
+
+        tabs.addTab(new WaTab<>().setPanel("guicedPanel").add(new WaIcon("home")).add("GuicedEE"),
+                new WaTabPanel<>().setName("guicedPanel").addStyle("height:100%").add(page), true);
+
+        tabs.addTab(new WaTab<>().setPanel("jwebPanel").add(new WaIcon("globe")).add("JWebMP"),
+                new WaTabPanel<>().setName("jwebPanel").addStyle("height:100%").add("JWebMP Content"), false);
+
+        tabs.addTab(new WaTab<>().setPanel("entityasssitPanel").add(new WaIcon("database")).add("EntityAssist"),
+                new WaTabPanel<>().setName("entityasssitPanel").addStyle("height:100%").add("EntityAssist Content"), false);
+
+        tabs.addTab(new WaTab<>().setPanel("activityMasterPanel").add(new WaIcon("tasks")).add("ActivityMaster"),
+                new WaTabPanel<>().setName("activityMasterPanel").addStyle("height:100%").add("ActivityMaster Content"), false);
+
+        add(tabs);
+
     }
 }
-//what port? 4200
-//perfect looks like its working
-
-//okkk, this brings in a new avenue for the angular plugin, removing the use of the boot run for client only oooo
-
-
-//for now just keep this running in ng serve, your updates will filter through, i'll sort out the angular plugin
-
-//Alright
-
-
-
-
-//The test classes are the best placed to see how to use components (y)
-//im assuming because test classes are a safer enviroment? Yeah - and because thats how we confirm the component works as well
-//so we test each component in a test class? - Yes
-// It also shows us to configure the java components properly --- I think
-//is there a seperate folder that is used for testing? (all maven has src/main/java - and src/test/java )
-//so what do I need to do right now or am i still in the learning phase? You can do the ->
-
-
-//So like tab icons, colours names, sizing, and then the url links for jwebmp, entity assist, activity master
-// Tabs are -> GuicedEE  -> JWebMP -> Entity Assist -> Activity Master
-//We are only doing the GuicedEE site now
-
-//Then the left hand (nagivation-menu) per item instead of whatever it is now (don't worry about splitting content here. this is the scaffolding phase)
-//And the burger menu when the site is small (wa-page has special handling for this)
-
-//Ok so for now just edit the tabs and add a navigation meny on the left hand side - yes, although i think it is called navigation-menu in wa-page, or similar
-
