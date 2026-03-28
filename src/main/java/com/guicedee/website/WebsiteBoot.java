ac1657855e85ddb5acf879119861c866ac58f2da
TheBlueHorizon <dtweninger81@gmail.com>
2026-03-28 12:46:03 +0200

    Remove outdated CSS reference in project diff state
    The commit removes a reference to an old CSS file in the `.idea/copilotDiffState.xml` IDE configuration. This cleanup eliminates unnecessary metadata to keep the project configuration files up-to-date and consistent.

--- src/main/java/com/guicedee/website/WebsiteBoot.java
+++ src/main/java/com/guicedee/website/WebsiteBoot.java
@@ -4,12 +4,15 @@ import com.jwebmp.core.base.angular.client.annotations.angular.NgComponent;
 import com.jwebmp.core.base.angular.client.annotations.routing.NgRoutable;
 import com.jwebmp.core.base.angular.client.services.interfaces.INgComponent;
 import com.jwebmp.core.base.angular.services.RouterOutlet;
+import com.jwebmp.core.base.html.Div;
 import com.jwebmp.core.base.html.DivSimple;
+import com.jwebmp.webawesome.components.Size;
 import com.jwebmp.webawesome.components.button.WaButton;
 import com.jwebmp.webawesome.components.button.WaDropDown;
 import com.jwebmp.webawesome.components.button.WaDropdownItem;
-import com.jwebmp.webawesome.components.divider.WaDivider;
 import com.jwebmp.webawesome.components.icon.WaIcon;
+import com.jwebmp.webawesome.components.input.InputSize;
+import com.jwebmp.webawesome.components.input.WaInput;
 import com.jwebmp.webawesome.components.tabgroup.WaTab;
 import com.jwebmp.webawesome.components.tabgroup.WaTabGroup;
 import com.jwebmp.webawesome.components.tabgroup.WaTabPanel;
@@ -17,11 +20,6 @@ import com.jwebmp.webawesomepro.components.page.WaPage;
 
 @NgComponent("guicedee-app")
 @NgRoutable(path = "")
-/**
- * Top Level = Tab Groups x 4, Only first has content
- * Tab Group Content is a WaPage with header, navigation, menu, main-header, main (as per docs)
- * The page has all the site details
- */
 public class WebsiteBoot extends DivSimple<WebsiteBoot> implements INgComponent<WebsiteBoot>
 {
     public WebsiteBoot()
@@ -35,15 +33,13 @@ public class WebsiteBoot extends DivSimple<WebsiteBoot> implements INgComponent<
         WaPage<?> page = new WaPage<>();
         page.setNavOpen(true);
 
-        var menu = page.getMenu();
-        menu.add("Home");
-
-        var navigation = page.getNavigation();
-        navigation.add("GuicedEE Navigation Links");
-
         var header = page.getHeader();
         WaDropDown<?> navDropdown = new WaDropDown<>();
-        navDropdown.setButton(new WaButton<>().add(new WaIcon("menu")));
+        WaButton<?> dropdownButton = new WaButton<>();
+        dropdownButton.add(new WaIcon<>("menu"));
+        dropdownButton.addStyle("background: none; border: none; padding: 0; color: inherit;");
+        dropdownButton.setSize(Size.Small);
+        navDropdown.setButton(dropdownButton);
         navDropdown.addItem(new WaDropdownItem<>("Home"));
         navDropdown.addItem(new WaDropdownItem<>("Modules"));
         navDropdown.addItem(new WaDropdownItem<>("Documentation"));
@@ -56,31 +52,37 @@ public class WebsiteBoot extends DivSimple<WebsiteBoot> implements INgComponent<
         navDropdown.addItem(new WaDropdownItem<>("Downloads"));
         navDropdown.addItem(new WaDropdownItem<>("FAQ"));
         navDropdown.addStyle("max-height: 250px; overflow-y: auto;");
-        header.add(navDropdown);
-        header.setSplit();
-        header.add("GuicedEE Website");
-
-        var mainHeader = page.getMainHeader();
-        var main = page.getMain();
 
         // Tabs
         WaTabGroup<?> tabs = new WaTabGroup<>()
-                .addStyle("width:50%")
-                .addStyle("height:50%");
+                .addStyle("width: auto;");
 
-        tabs.addTab(new WaTab<>().setPanel("guicedPanel").add(new WaIcon("home")).add("GuicedEE"),
+        tabs.addTab(new WaTab<>().setPanel("guicedPanel").add(new WaIcon<>("home")).add("GuicedEE"),
                 new WaTabPanel<>().setName("guicedPanel").addStyle("height:100%").add(new RouterOutlet()), true);
 
-        tabs.addTab(new WaTab<>().setPanel("jwebPanel").add(new WaIcon("globe")).add("JWebMP"),
+        tabs.addTab(new WaTab<>().setPanel("jwebPanel").add(new WaIcon<>("globe")).add("JWebMP"),
                 new WaTabPanel<>().setName("jwebPanel").addStyle("height:100%").add("JWebMP Content"), false);
 
-        tabs.addTab(new WaTab<>().setPanel("entityasssitPanel").add(new WaIcon("database")).add("EntityAssist"),
+        tabs.addTab(new WaTab<>().setPanel("entityasssitPanel").add(new WaIcon<>("database")).add("EntityAssist"),
                 new WaTabPanel<>().setName("entityasssitPanel").addStyle("height:100%").add("EntityAssist Content"), false);
 
-        tabs.addTab(new WaTab<>().setPanel("activityMasterPanel").add(new WaIcon("tasks")).add("ActivityMaster"),
+        tabs.addTab(new WaTab<>().setPanel("activityMasterPanel").add(new WaIcon<>("tasks")).add("ActivityMaster"),
                 new WaTabPanel<>().setName("activityMasterPanel").addStyle("height:100%").add("ActivityMaster Content"), false);
 
-        main.add(tabs);
+        header.add(navDropdown);
+        header.add(tabs);
+
+        header.setSplit();
+
+        WaInput<?> search = new WaInput<>().setPlaceholder("Search...");
+        search.setSize(InputSize.SMALL);
+        search.addStyle("margin: 0 10px;");
+        header.add(search);
+
+        WaButton<?> themeToggle = new WaButton<>().add(new WaIcon<>("moon"));
+        themeToggle.setSize(Size.Small);
+        themeToggle.addStyle("background: none; border: none; padding: 0; color: inherit; margin-right: 15px;");
+        header.add(themeToggle);
 
         add(page);
 
